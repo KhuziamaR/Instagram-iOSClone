aa603259a839491a6166671c183e374cac795919
KhuziamaR <krehman000@citymail.cuny.edu>
2021-03-19 22:09:28 -0400

    fixed image bug

--- Instagram-iOSClone/CameraViewController.swift
+++ Instagram-iOSClone/CameraViewController.swift
@@ -27,7 +27,7 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         post["author"] = PFUser.current()!
         
         let imageData = imageView.image!.pngData()
-        let file = PFFileObject(data: imageData!)
+        let file = PFFileObject(name: "image.png", data: imageData!)
         
         post["image"] = file
         
